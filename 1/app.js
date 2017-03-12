5e89b790041079dd9cbb63093115a16934c98af1
Mikhail Novikov <mixail.novikov@gmail.com>
2017-03-13 00:26:12 +0300

    remove unnecessary function

--- 1/app.js
+++ 1/app.js
@@ -1,16 +1,10 @@
-function throttleArray(array=[], timeout=1000) {
-  outputArray(array, timeout);
-}
-
-function outputArray(array, timeout, index=1) {
+function throttleArray(array=[], timeout=1000, index=1) {
   if (array.length >= index) {
       setTimeout(function() {
         console.log(array.slice(0, index).join(', '));
-        outputArray(array, timeout, index + 1);
+        throttleArray(array, timeout, index + 1);
       }, timeout);
   }
 }
 
-var inputArray = ['aaa', 'bbb', 'xxx', 'ddd', 'zzz'];
-
-throttleArray(inputArray);
+throttleArray(['aaa', 'bbb', 'xxx', 'ddd', 'zzz']);
